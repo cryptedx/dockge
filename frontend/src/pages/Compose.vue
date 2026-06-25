ed6a354f68adbf49ddb26f7df7f9c004ae124f0f
crypted <crypted@me.com>
2026-06-25 10:46:28 +0200

    Revert "fix: align compose details pane scrolling"
    This reverts commit d1d89edbacb2c072f429b36582237d1a9a131e0d.

--- frontend/src/pages/Compose.vue
+++ frontend/src/pages/Compose.vue
@@ -142,7 +142,7 @@
                         v-show="!composeFocusMode"
                         id="compose-details-pane"
                         class="compose-details-pane"
-                        :style="detailsPaneStyle"
+                        :style="{ width: `${detailsPaneWidth}px` }"
                     >
                         <div v-if="isAdd" class="compose-details-section">
                             <h4 class="mb-3">{{ $t("general") }}</h4>
@@ -497,7 +497,6 @@ export default {
             isResizingDetailsPane: false,
             composeFocusMode: false,
             windowWidth: window.innerWidth,
-            windowTop: 0,
             detailsTab: "containers",
             updateCheck: null,
             updateScanRunning: false,
@@ -511,22 +510,6 @@ export default {
 
         showCombinedTerminalPanel() {
             return !this.isEditMode && !this.composeFocusMode;
-        },
-
-        detailsPaneStyle() {
-            const style = {
-                width: `${this.detailsPaneWidth}px`,
-            };
-
-            if (this.windowWidth > 550) {
-                return {
-                    ...style,
-                    height: `calc(100vh - 160px + ${this.windowTop}px)`,
-                };
-            }
-
-            style.height = "calc(100vh - 160px)";
-            return style;
         },
 
         endpointDisplay() {
@@ -710,8 +693,6 @@ export default {
         this.requestDockerStats();
         this.scheduleCombinedTerminalPanelBoundsUpdate();
         window.addEventListener("resize", this.updateWindowWidth);
-        window.addEventListener("scroll", this.onScroll);
-        this.onScroll();
         window.addEventListener("keydown", this.handleGlobalTerminalShortcut);
     },
     updated() {
@@ -728,18 +709,9 @@ export default {
         this.combinedTerminalPanelResizeObserver = null;
         this.combinedTerminalPanelResizeAnchor = null;
         window.removeEventListener("resize", this.updateWindowWidth);
-        window.removeEventListener("scroll", this.onScroll);
         window.removeEventListener("keydown", this.handleGlobalTerminalShortcut);
     },
     methods: {
-        onScroll() {
-            if (window.top.scrollY <= 133) {
-                this.windowTop = window.top.scrollY;
-            } else {
-                this.windowTop = 133;
-            }
-        },
-
         updateWindowWidth() {
             this.windowWidth = window.innerWidth;
             this.scheduleCombinedTerminalPanelBoundsUpdate();
@@ -1335,10 +1307,7 @@ export default {
 .compose-details-pane {
     flex: 0 0 auto;
     min-width: 0;
-    overflow-y: auto;
     padding-left: 12px;
-    position: sticky;
-    top: 10px;
 }
 
 .compose-details-section {
@@ -1513,10 +1482,7 @@ export default {
     }
 
     .compose-details-pane {
-        height: auto !important;
-        overflow-y: visible;
         padding-left: 0;
-        position: static;
         width: auto !important;
     }
 
@@ -1570,10 +1536,7 @@ export default {
     }
 
     .compose-details-pane {
-        height: auto !important;
-        overflow-y: visible;
         padding-left: 0;
-        position: static;
         width: auto !important;
     }
 
